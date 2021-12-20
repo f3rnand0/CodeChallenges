a2321b5a256a9de8a807b34e395d7820e9c9140c
Fernando Guerra <fgucutito@gmail.com>
2021-12-20 15:20:22 -0500

    Added new challenges

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,7 @@ r
 Bjdk11/src/main/java/technical/challenges/BlastRadiusChallenge.java,d/c/dc3014a8bf9875701326911ef725c2eec8073c44
 u
 Ejdk11/src/main/java/technical/challenges/CloudStack360Challenge1.java,1/3/13c582861edd09eeaa113843d9a639ca8a6eb905
+p
+@jdk11/src/main/java/technical/challenges/NordstromChallenge.java,f/f/fffbb427694bc57ae606ef5e274df26c3020984b
+n
+>jdk11/src/main/java/technical/challenges/TestsExpressions.java,4/d/4dba6dac6d9629daef5f77ef06d497c4b5d5dd14
---
+++ jdk11/src/main/java/technical/challenges/TestsExpressions.java
@@ -0,0 +1,66 @@
+package technical.challenges;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class TestsExpressions {
+    public static void main(String[] args) {
+        //System.out.println(TestsExpressions.caesarCypherEncryptor("xyz", 2));
+        System.out.println("AND");
+        System.out.println("false && false: " + (false && false));
+        System.out.println("false && true: " + (false && true));
+        System.out.println("true && false: " + (true && false));
+        System.out.println("true && true: " + (true && true));
+        System.out.println();
+        System.out.println("OR");
+        System.out.println("false || false: " + (false || false));
+        System.out.println("false || true: " + (false || true));
+        System.out.println("true || false: " + (true || false));
+        System.out.println("true || true: " + (true || true));
+        System.out.println(3/2);
+        List<Integer[]> list = new ArrayList();
+        list.add(new Integer[]{1,2});
+        list.remove(2);
+        list.set(1, new Integer[]{1,2});
+        list.add(1, new Integer[]{1,2});
+        List<Integer> list1 = new ArrayList<>();
+        Collections.sort(list1);
+        list1.indexOf(1);
+    }
+
+    public static String longestPalindromicSubstring(String str) {
+        char[] array = str.toCharArray();
+        List<String> list = new ArrayList<>();
+        String largestPalindrome = str.substring(0,0);
+        for(int i=0; i < array.length; i++) {
+            for(int j=i+1; j < array.length; j++) {
+                String subStr = str.substring(i, j);
+                if (isPalindrome(subStr))
+                    list.add(subStr);
+            }
+        }
+        int length=0;
+        for(String p : list){
+            if (length < p.length()) {
+                length = p.length();
+                largestPalindrome = p;
+            }
+        }
+        return largestPalindrome;
+    }
+
+    private static boolean isPalindrome(String str) {
+        str.substring(0,1);
+        int i=0, j = str.length() - 1;
+        if (str.length() == 1)
+            return true;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
